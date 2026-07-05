0ca1fb334939b8f09991cc405e5b30b367b9bf72
SaudG5 <ssssgg9000@gmial.com>
2026-07-05 16:25:19 +0000

    add scripte that prints using puts funcion

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+
+/**
+ * main - prints a sentence followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
