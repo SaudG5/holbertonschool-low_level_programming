deb68d3ea1cb10e8fc4daa576c7698224d50346f
SaudG5 <ssssgg9000@gmail.com>
2026-07-06 14:06:23 +0000

    prints all numbers for 0 to 10

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - print the numbers from 0 to 9
+ *
+ * Return: alwayes 0
+ */
+int main(void)
+{
+for(int i=0;i<10;i++)
+{
+prinf("%d",i);
+}
+putchar('\n');
+return(0);
