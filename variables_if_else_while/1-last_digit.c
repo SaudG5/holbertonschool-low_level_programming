eca9ca40f478cb376a8ce2914eb3d4577c9fd1a7
SaudG5 <ssssgg9000@gmail.com>
2026-07-06 12:15:53 +0000

    add script that detrmain if the last digit of a random number will be >5 or less than 6 or equal 0

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+/* main - take random number and detrmain if the last digit its greater than 5 or less than 5 or equal 0
+ *
+ *return - alwayes 0 with massages */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if ((n%10)>5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n",n,n%10);
+	}
+	else if ((n%10) == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n",n,n%10);
+	}
+	else if ((n%10)<6)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n",n,n%10);
+	}
+	return (0);
+}
