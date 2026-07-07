e8d7f3c4e0da352b11a3419686a40ebf1a22a2cc
SaudG5 <you@example.com>
2026-07-07 17:48:44 +0300

    add script to prints 16 base numbers

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - prints all numbers of base 16 in lowercase
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	while (i < 16)
+	{
+		if (i < 10)
+			putchar('0' + i);
+		else
+			putchar('a' + (i - 10));
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
