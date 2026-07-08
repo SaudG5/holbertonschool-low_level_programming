e91b5600c6a4e55091e2580ae5e699b451d5704e
SaudG5 <ssssgg9000@gmail.com>
2026-07-08 19:12:22 +0300

    Add task _putchar.c file, update README and main.h files

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,11 +1,22 @@
 #include <stdio.h>
+
+int _putchar(char c);
+
 /**
- *main - print _putchar
+ * main - prints _putchar followed by a new line
  *
- * Return: alwayes 0
+ * Return: always 0
  */
 int main(void)
 {
-	puts("_putchar");
-	return (0);
+    _putchar('_');
+    _putchar('p');
+    _putchar('u');
+    _putchar('t');
+    _putchar('c');
+    _putchar('h');
+    _putchar('a');
+    _putchar('r');
+    _putchar('\n');
+    return (0);
 }
