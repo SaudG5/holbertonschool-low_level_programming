cd228f01e885dbe3fa887d607631d91709ddfee7
SaudG5 <ssssgg9000@gmail.com>
2026-07-08 21:54:15 +0300

    Add task 2 file, update README and main.h files

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,23 +1,24 @@
 #include "main.h"
 
 /**
- * print_alphabet - prints the lowercase alphabet 10 times, followed by a newline
+ * print_alphabet_x10 - prints the lowercase alphabet 10 times,
+ * each followed by a newline
  *
  * Return: void
  */
 void print_alphabet_x10(void)
 {
 	int b = 0;
+	int i;
+
 	while (b < 10)
 	{
-		int i = 0;
-
+		i = 0;
 		do {
-				_putchar('a' + i);
-				i++;
+			_putchar('a' + i);
+			i++;
 		} while (i < 26);
-
-	_putchar('\n');
-	b++;
+		_putchar('\n');
+		b++;
 	}
 }
