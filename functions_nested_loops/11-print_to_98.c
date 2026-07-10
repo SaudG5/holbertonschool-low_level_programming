5c030dab4caf1317d554c86904e0df4a93262c9b
SaudG5 <ssssgg9000.com>
2026-07-10 19:28:39 +0300

    Add task 11 file, update README and main.h files

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * print_to_98 - count any nymber to 98
+ * @n: number that conut start from
+ *
+ * Return: the counts from n to 98
+ */
+void  print_to_98(int n)
+{
+	if (n > 98)
+	{
+		while (n > 98)
+		{
+			printf("%d, ", n);
+			n = n - 1;
+		}
+		printf("%d\n", n);
+		return;
+	}
+	else if (n < 98)
+	{
+		while (n < 98)
+		{
+			printf("%d, ", n);
+			n = n + 1;
+		}
+		printf("%d\n", n);
+		return;
+	}
+	else
+	{
+		printf("%d\n", n);
+		return;
+	}
+}
