3d482e5abbedd24b7d776d48db621e2f654aefba
SaudG5 <ssssgg9000.com>
2026-07-10 18:43:45 +0300

    Add task 10 file, update README and main.h files

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - add to int number
+ * @i: the first integer
+ * @b: the second integer
+ *
+ * Return: return the two added numbers
+ */
+int add(int i, int b)
+{
+	int a = i + b;
+
+	return (a);
+}
