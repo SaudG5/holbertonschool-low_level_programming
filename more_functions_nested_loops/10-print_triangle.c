1c2bdac7a1ef1365072344261a495feb15a3b40f
SaudG5 <ssssgg9000.com>
2026-07-11 15:47:15 +0300

    Add task 10 file, update README and main.h files

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle using the # character
+ * @size: the size of the triangle
+ *
+ * Return: void
+ */
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	for (i = 1; i <= size; i++)
+	{
+		for (j = 0; j < size - i; j++)
+			_putchar(' ');
+
+		for (j = 0; j < i; j++)
+			_putchar('#');
+
+		_putchar('\n');
+	}
+}
