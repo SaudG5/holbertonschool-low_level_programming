209abc47ffa5248363a5f650a56addddcb20c71b
SaudG5 <ssssgg9000.com>
2026-07-11 14:02:47 +0300

    Add task 2 file, update README and main.h files

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - multiplies to integrs
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: return the multiplication value
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
---
+++ more_functions_nested_loops/_putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _putchar - writes a character to stdout
+ * @c: the character to print
+ *
+ * Return: always 1
+ */
+int _putchar(char c)
+{
+	return (putchar(c));
+}
