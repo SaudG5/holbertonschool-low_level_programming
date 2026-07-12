8f0f91759c2e7b82035818a59758d0b913907f3f
SaudG5 <ssssgg9000.com>
2026-07-12 16:10:27 +0300

    Add task calculator.c file, update README and main.h files

--- simple-calculator/calculator.c
+++ simple-calculator/calculator.c
@@ -1,19 +1,24 @@
 #include <stdio.h>
 int main(void)
 {
-	int choice;
-
-	printf("Welcome to the simple calcutar, which operation do you want?\n");
-	printf("1 Add\n");
-	printf("2 Subtract\n");
-	printf("3 Multiply\n");
-	printf("4 Divide\n");
-	printf("0 Quit\n");
-	printf("choose the number of operatoin you want: \n");
-	scanf("%d", &choice);
-	if (choice == 0)
+	while (True)
 	{
-		printf("Bye!\n");
+		int choice;
+
+		printf("Welcome to the simple calcutar, which operation do you want?\n");
+		printf("1 Add\n");
+		printf("2 Subtract\n");
+		printf("3 Multiply\n");
+		printf("4 Divide\n");
+		printf("0 Quit\n");
+		printf("choose the number of operatoin you want: \n");
+		scanf("%d", &choice);
+		if (choice == 0)
+		{
+			printf("Bye!\n");
+			return (0);
+		}
+		else if (choice > 4 && choice < 0)
+			printf("Invalid choice");
 	}
-	return (0);
 }
