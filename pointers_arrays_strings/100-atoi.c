e0fe21531ed1b597bce8a9d2e3726bb153c77330
SaudG5 <ssssgg9000@gmail.com>
2026-07-15 16:47:18 +0300

    add function that print the number with the sighn

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,3 +1,16 @@
+#include "main.h"
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: the string to be converted
+ *
+ * Description: the number in the string can be preceded by any
+ * number of non-digit characters. Every '-' encountered before the
+ * first digit flips the sign; '+' and all other characters have no
+ * effect on the sign. If no digits are found, 0 is returned.
+ *
+ * Return: the converted integer
+ */
 int _atoi(char *s)
 {
 	int i = 0;
@@ -13,9 +26,9 @@ int _atoi(char *s)
 
 	while (s[i] >= '0' && s[i] <= '9')
 	{
-		num = num * 10 + (s[i] - '0');
+		num = num * 10 - (s[i] - '0');
 		i++;
 	}
 
-	return (sign * num);
+	return (sign == -1 ? num : -num);
 }
