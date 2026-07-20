9736d9ca086051a580eb2f48ae2aaed574765864
SaudG5 <ssssgg9000@gmail.com>
2026-07-20 15:38:33 +0300

    fixed strdup null check

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strdup - function returns a pointer to a new string which is a duplicate of the string str
+ * @str: pointer
+ *
+ * Return: NULL if str = NULL or insufficient memory, on success function returns a pointer to the duplicated string
+ */
+char *_strdup(char *str)
+{
+	int len;
+	char *copy;
+	int i;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[len] != '\0')
+		len++;
+		
+	copy = malloc(sizeof(char) * (len + 1));
+	
+	if (copy == NULL)
+		return (NULL);
+
+	for (i = 0; i < len; i++)
+		copy[i] = str[i];
+
+	copy[len] = '\0';
+	return (copy);
+}
