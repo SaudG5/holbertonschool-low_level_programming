0fa01cdb31f43cb7ab901284fcc04fc7ebbdfbd2
SaudG5 <ssssgg9000.com>
2026-07-21 16:18:34 +0300

    add sol

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -12,39 +12,39 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-        unsigned int len1 = 0, len2 = 0, i = 0, j = 0;
-        char *contact;
+	unsigned int len1 = 0, len2 = 0, i = 0, j = 0;
+	char *contact;
 
-        if (s1 == NULL)
-                s1 = "";
-        if (s2 == NULL)
-                s2 = "";
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-        while (s1[len1] != '\0')
-                len1++;
+	while (s1[len1] != '\0')
+		len1++;
 
-        while (s2[len2] != '\0')
-                len2++;
+	while (s2[len2] != '\0')
+		len2++;
 
-        if (n >= len2)
-                n = len2;
+	if (n >= len2)
+		n = len2;
 
-        contact = malloc(sizeof(char) * (len1 + n + 1));
+	contact = malloc(sizeof(char) * (len1 + n + 1));
 
-        if (contact == NULL)
-                return (NULL);
+	if (contact == NULL)
+		return (NULL);
 
-        for (i = 0; i < len1; i++)
-        {
-                contact[i] = s1[i];
-        }
+	for (i = 0; i < len1; i++)
+	{
+		contact[i] = s1[i];
+	}
 
-        for (j = 0; j < n; j++, i++)
-        {
-                contact[i] = s2[j];
-        }
+	for (j = 0; j < n; j++, i++)
+	{
+		contact[i] = s2[j];
+	}
 
-        contact[i] = '\0';
+	contact[i] = '\0';
 
-        return (contact);
+	return (contact);
 }
