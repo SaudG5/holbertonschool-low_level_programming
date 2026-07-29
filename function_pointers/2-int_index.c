081e1e11a39a07f35866ba8e594a2c1863ecfb62
SaudG5 <ssssgg9000.com>
2026-07-29 16:20:21 +0300

    add function that search for when cmp dont return 0

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -1,3 +1,4 @@
+#include <stddef.h>
 #include "function_pointers.h"
 
 /**
@@ -7,13 +8,13 @@
  * @cmp: pointer to the function to use to compare values
  *
  * Return: the index of the first element for which cmp does not
- * return 0, or -1 if no element matches or if size <= 0
+ * return 0, or -1 if no element matches, array is NULL, or size <= 0
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size <= 0)
+	if (array == NULL || size <= 0)
 		return (-1);
 
 	for (i = 0; i < size; i++)
