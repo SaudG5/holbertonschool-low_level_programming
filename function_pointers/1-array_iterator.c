54afa2803cae1651c52c6d849a15ed2f9f63f5b5
SaudG5 <ssssgg9000.com>
2026-07-29 15:18:48 +0300

    add a funtion that take an array and prpint every element using anthor function

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - executes a function given as a parameter on
+ * each element of an array
+ * @array: the array to iterate through
+ * @size: the size of the array
+ * @action: pointer to the function to use on each element
+ *
+ * Return: Nothing.
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
