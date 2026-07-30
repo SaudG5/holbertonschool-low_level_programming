1c4871c008a11b4fdf49e1c5ed0bdcbb33ca00fa
SaudG5 <ssssgg9000.com>
2026-07-30 15:34:10 +0300

    realate to betty style

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -1,39 +1,32 @@
 #include "variadic_functions.h"
 
 /**
- * sum_them_all - Sums a variable number of unsigned int arguments.
+ * sum_them_all - sums a variable number of unsigned int arguments
+ * @n: the number of variadic arguments that follow
+ * @...: a variable-length argument list containing exactly n
+ * unsigned integers
  *
- * @n: The number of variadic arguments that follow. Since it is declared
- *     const, the function must not modify it.
- * @...: A variable-length argument list containing exactly n unsigned
- *       integers, to be accessed via the <stdarg.h> macros
- *       (va_list, va_start, va_arg, va_end).
- *
- * Description: If n is 0, the function returns 0 immediately without
- * touching the variadic list. Otherwise, it iterates n times, extracting
- * one unsigned int per iteration and accumulating them into a running
- * total.
- *
- * Return: An int representing the sum of all n arguments passed in.
+ * Return: an int representing the sum of all n arguments passed in,
+ * or 0 if n is 0
  */
 int sum_them_all(const unsigned int n, ...)
 {
-    int sum;
-    unsigned int i;
-    va_list args;
+	va_list args;
+	int sum;
+	unsigned int i;
 
-    va_start(args, n);
+	va_start(args, n);
 
-    if (n == 0)
-    {
+	if (n == 0)
+	{
+		va_end(args);
+		return (0);
+	}
+
+	sum = 0;
+	for (i = 0; i < n; i++)
+		sum += va_arg(args, unsigned int);
+
 	va_end(args);
-	return (0);
-    }
-
-    sum = 0;
-    for (i = 0; i < n; i++)
-	sum += va_arg(args, unsigned int);
-
-    va_end(args);
-    return (sum);
+	return (sum);
 }
