5b29dddc6603a29bcd0b8e45c79a82b1d7a7be2d
SaudG5 <ssssgg9000.com>
2026-07-30 16:06:16 +0300

    print each argument

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -25,9 +25,9 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%d", va_arg(args, unsigned int));
 		if (i != 0 && separator != NULL)
 			printf("%s", separator);
+		printf("%d", va_arg(args, unsigned int));
 	}
 	va_end(args);
 	printf("\n");
