5c42be5a8a929c088982c5babad061e7fef50d70
SaudG5 <ssssgg9000.com>
2026-08-09 16:22:36 +0300

    add

--- singly_linked_lists/100-first.c
+++ singly_linked_lists/100-first.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
+void first(void) __attribute__((constructor));
+
 /**
  * first - prints a message before main() is executed
  */
-__attribute__((constructor)) void first(void)
+void first(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
