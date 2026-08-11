70b54adad868764564795e034383448440465537
SaudG5 <ssssgg9000.com>
2026-08-11 18:59:36 +0300

    Edit file

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 #include <stdio.h>
- 
+
 /**
  * print_dlistint - prints all the elements of a dlistint_t list
  * @h: pointer to the head of the list
@@ -9,14 +9,14 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t count = 0;
- 
-	while (h != NULL)
-	{
-		printf("%d\n", h->n);
-		h = h->next;
-		count++;
-	}
- 
-	return (count);
+size_t count = 0;
+
+while (h != NULL)
+{
+printf("%d\n", h->n);
+h = h->next;
+count++;
+}
+
+return (count);
 }
