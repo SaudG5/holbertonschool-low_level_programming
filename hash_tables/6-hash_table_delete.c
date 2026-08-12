5ab635f4561ba29edeb181a637c718ea325df28e
SaudG5 <ssssgg9000.com>
2026-08-12 10:52:38 +0300

    Task 6

---
+++ hash_tables/6-hash_table_delete.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - Deletes a hash table
+ * @ht: The hash table to delete
+ *
+ * Description: Frees every node's key and value, then every node,
+ * then the array, then the hash table struct itself
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *tmp, *next;
+
+	if (ht == NULL)
+		return;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		tmp = ht->array[i];
+		while (tmp != NULL)
+		{
+			next = tmp->next;
+			free(tmp->key);
+			free(tmp->value);
+			free(tmp);
+			tmp = next;
+		}
+	}
+
+	free(ht->array);
+	free(ht);
+}
