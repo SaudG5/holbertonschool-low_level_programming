50fdfde8f20c7a410e438cbf1ba8c1cbd8ce5561
SaudG5 <ssssgg9000.com>
2026-08-16 19:07:24 +0300

    Edit file to be in Betty style

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -6,6 +6,10 @@
 
 #define BUF_SIZE 1024
 
+/**
+ * close_fd - closes a file descriptor, exits with 100 on failure
+ * @fd: the file descriptor to close
+ */
 void close_fd(int fd)
 {
 	if (close(fd) == -1)
@@ -15,31 +19,41 @@ void close_fd(int fd)
 	}
 }
 
-int main(int argc, char *argv[])
+/**
+ * open_files - opens file_from for reading and file_to for writing
+ * @file_from: name of the file to read from
+ * @file_to: name of the file to write to
+ * @fd_from: pointer to store the file_from file descriptor
+ * @fd_to: pointer to store the file_to file descriptor
+ */
+void open_files(char *file_from, char *file_to, int *fd_from, int *fd_to)
 {
-	int fd_from, fd_to;
-	ssize_t bytes_read, bytes_written;
-	char buf[BUF_SIZE];
-
-	if (argc != 3)
+	*fd_from = open(file_from, O_RDONLY);
+	if (*fd_from == -1)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
-
-	fd_from = open(argv[1], O_RDONLY);
-	if (fd_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
 		exit(98);
 	}
 
-	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (fd_to == -1)
+	*fd_to = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (*fd_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
 		exit(99);
 	}
+}
+
+/**
+ * copy_content - copies content from fd_from to fd_to
+ * @fd_from: source file descriptor
+ * @fd_to: destination file descriptor
+ * @file_from: name of the source file (for error messages)
+ * @file_to: name of the destination file (for error messages)
+ */
+void copy_content(int fd_from, int fd_to, char *file_from, char *file_to)
+{
+	ssize_t bytes_read, bytes_written;
+	char buf[BUF_SIZE];
 
 	bytes_read = read(fd_from, buf, BUF_SIZE);
 	while (bytes_read > 0)
@@ -47,7 +61,7 @@ int main(int argc, char *argv[])
 		bytes_written = write(fd_to, buf, bytes_read);
 		if (bytes_written == -1 || bytes_written != bytes_read)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
 			exit(99);
 		}
 		bytes_read = read(fd_from, buf, BUF_SIZE);
@@ -55,9 +69,30 @@ int main(int argc, char *argv[])
 
 	if (bytes_read == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
 		exit(98);
 	}
+}
+
+/**
+ * main - copies the content of a file to another file
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int fd_from, fd_to;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	open_files(argv[1], argv[2], &fd_from, &fd_to);
+	copy_content(fd_from, fd_to, argv[1], argv[2]);
 
 	close_fd(fd_from);
 	close_fd(fd_to);
